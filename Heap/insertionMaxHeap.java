241b5aa41a2d3d0e31c39b49904c1915010d7101
Sravan Surya <vsravansurya@gmail.com>
2024-12-18 18:34:06 +0530

    Insertion in a  MaxHeap

---
+++ Heap/insertionMaxHeap.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class insertionMaxHeap {
+
+    static void swap(List<Integer> heap ,int l,int r){
+        int temp = heap.get(l);
+        heap.set(l,heap.get(r));
+        heap.set(r,temp);
+    }
+
+    static void  push_up_in_maxHeap(List<Integer> heap,int i){
+        int parentIdx = (i-1)/2;
+        if(i==0 || heap.get(parentIdx) > heap.get(i)) return;
+
+        swap(heap,parentIdx,i);
+        push_up_in_maxHeap(heap,parentIdx);
+    }
+    static void insert_in_MaxHeap(List<Integer> heap,int element){
+        heap.add(element);
+
+        int index = heap.size()-1;
+
+        push_up_in_maxHeap(heap,index);
+
+    }
+    public static void main(String[] args) {
+        List<Integer> heap = new ArrayList<>();
+        heap.add(60);
+        heap.add(50);
+        heap.add(40);
+        heap.add(30);
+        heap.add(20);
+        heap.add(30);
+        heap.add(10);
+        System.out.println(heap);
+        insert_in_MaxHeap(heap,55);
+        System.out.println(heap);
+    }
+}
